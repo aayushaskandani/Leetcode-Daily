e9e051e9a3b741b02fc3be359262b06d050c6292
Aayush Askandani <aayushaskandani64@gmail.com>
2026-07-16 22:30:24 +0530

    Time: 3 ms (99.78%), Space: 47.4 MB (21.91%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,42 +1,37 @@
 class Solution {
+    public boolean dfs(int node,ArrayList<ArrayList<Integer>> adj, boolean[] vis, boolean[] pathVis){
+        vis[node]=true;
+        pathVis[node]=true;
+        for(int neighbor : adj.get(node)){
+            if(!vis[neighbor]){
+                if(dfs(neighbor,adj,vis,pathVis)) return true;
+
+            }else if(pathVis[neighbor]) return true;
+
+        }
+        pathVis[node]=false;
+        return false;
+    }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<numCourses;i++){
+        for(int i =0;i<numCourses;i++){
             adj.add(new ArrayList<>());
 
         }
-        for(int[] pre:prerequisites){
-            int a=pre[0];
-            int b=pre[1];
-            adj.get(b).add(a);
+        for(int[] pre: prerequisites){
+            adj.get(pre[1]).add(pre[0]);
 
         }
-        int[] indegree=new int[numCourses];
-        for(int i=0;i<numCourses;i++){
-            for(int neighbor:adj.get(i)){
-                indegree[neighbor]++;
+        boolean[] vis = new boolean[numCourses];
+        boolean[] pathVis = new boolean[numCourses];
 
-            }
-        }
-        Queue <Integer> q= new LinkedList<>();
-       for(int i=0;i<numCourses;i++){
-        if(indegree[i]==0){
-            q.offer(i);
+        for(int i = 0;i<numCourses;i++){
+              if(!vis[i]){
+                if(dfs(i,adj,vis,pathVis)) return false;
 
+            }  
         }
-       } 
-       int count=0;
-        while(!q.isEmpty()){
-            int node=q.poll();
-            count++;
-            for(int neighbor:adj.get(node)){
-                indegree[neighbor]--;
-                if(indegree[neighbor]==0){
-                    q.offer(neighbor);
-
-                }
-            }
-        }
-        return count==numCourses;
+        return true;
+        
     }
 }
